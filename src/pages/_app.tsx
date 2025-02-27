19e9191b0ead4ff1c5e1fab1f5f113b99b6d3c46
lyss91 <leeyss1991@gmail.com>
2025-02-27 13:09:16 +0900

    25.02.27 라우터의 이해

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <Link href={"/"}>홈</Link>
+        &nbsp;
+        <Link href={"/search?keword=아이유"}>검색 /search?keword=아이유 </Link>
+        &nbsp;
+        <Link href={"/good/1"}>제품상세 /good/1 </Link>
+        &nbsp;
+        <button>홈으로 이동하기</button>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer></footer>
+    </>
+  );
 }
